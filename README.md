65255dcca6b8e9f857fdee1aa4e4374e98dd0c6e
LBruyne <519102931@qq.com>
2021-06-16 01:12:16 +0800

    项目搭建

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # recommend-system
+
 基于Spark和ElasticSearch搭建的商铺推荐平台
---
+++ src/test/java/com/hinsliu/dianping/DianpingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hinsliu.dianping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DianpingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
